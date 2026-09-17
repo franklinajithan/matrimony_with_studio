9d4789fb9a674951ee2d6240ce1f58cf1be12d5f
Ajithan Arulgnanaselvam <148918699+franklinajithan@users.noreply.github.com>
2026-09-17 22:17:58 +0100

    Add subscription error codes

---
+++ src/lib/subscriptions/errors.ts
@@ -0,0 +1,1 @@
+export const SUBSCRIPTION_ERRORS={unauthorized:'SUBSCRIPTION_UNAUTHORIZED',upgradeRequired:'UPGRADE_REQUIRED',limitReached:'MONTHLY_LIMIT_REACHED',billingUnavailable:'BILLING_UNAVAILABLE'} as const;
