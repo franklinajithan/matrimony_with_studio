64e5fbbacbdcd174a4abe8b80a2780884e0dc843
Ajithan Arulgnanaselvam <148918699+franklinajithan@users.noreply.github.com>
2026-09-25 21:35:40 +0100

    Use continuous animated opening story on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { redirect } from "next/navigation";
-import { LandingHero } from "@/components/landing/LandingHero";
-import { ConnectionScrollStory } from "@/components/landing/ConnectionScrollStory";
-import { TaglineReveal } from "@/components/landing/TaglineReveal";
+import { PremiumOpeningStory } from "@/components/landing/PremiumOpeningStory";
 import { CompatibilityStory, StickyHowItWorks } from "@/components/landing/PremiumScrollSections";
 import { GlobalCommunityStory, PremiumStories, PremiumFinalCta } from "@/components/landing/PremiumClosingSections";
-import { TrustStrip, DiscoveryPreview } from "@/components/landing/LandingSections";
 import { Navbar } from "@/components/navigation/Navbar";
 import { Footer } from "@/components/navigation/Footer";
 import { getServerAuthUser } from "@/lib/supabase/server";
@@ -17,11 +14,7 @@ export default async function HomePage() {
     <div className="flex min-h-screen flex-col bg-[#FBF8F4]">
       <Navbar />
       <main className="flex-grow">
-        <LandingHero />
-        <TrustStrip />
-        <TaglineReveal />
-        <DiscoveryPreview />
-        <ConnectionScrollStory />
+        <PremiumOpeningStory />
         <CompatibilityStory />
         <StickyHowItWorks />
         <GlobalCommunityStory />
